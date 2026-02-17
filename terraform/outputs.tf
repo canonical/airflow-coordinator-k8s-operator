4e2478555ce4323420d7140a6d2c78a4c5ae0457
jananisenthilkumar291 <janani382001@gmail.com>
2026-02-17 10:51:19 -0500

    Adding terraform module with test and CI

---
+++ terraform/outputs.tf
@@ -0,0 +1,16 @@
+# CC008: charm modules must output the deployed application object.
+output "application" {
+  value = juju_application.airflow_coordinator_k8s
+}
+
+output "provides" {
+  value = {
+    airflow_coordinator = "airflow-coordinator"
+  }
+}
+
+output "requires" {
+  value = {
+    postgres = "postgres"
+  }
+}
