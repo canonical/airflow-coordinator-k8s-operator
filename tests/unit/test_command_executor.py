41519fd54df6eb03cd340d2b201a9213e45a4653
jananisenthilkumar291 <janani382001@gmail.com>
2026-03-09 12:03:20 -0400

    Removed AIRFLOW_HOME env variable - update tests

--- tests/unit/test_command_executor.py
+++ tests/unit/test_command_executor.py
@@ -41,7 +41,6 @@ class TestCommandExecutor:
         mock_container.exec.assert_called_once_with(
             ["airflow", "db", "migrate"],
             environment={
-                "AIRFLOW_HOME": "/opt/airflow",
                 "AIRFLOW__CORE__DAGS_FOLDER": "/opt/airflow/dags",
             },
         )
