3335e20da86cd24a672846b62c226914f54017b2
jananisenthilkumar291 <janani382001@gmail.com>
2026-08-21 11:23:47 -0400

    fix: use .get() instead of directly accessing the key tls_ca_chain

--- src/connection_manager.py
+++ src/connection_manager.py
@@ -78,9 +78,10 @@ class S3ConnectionInfo:
 
         normalized_data = {key.replace("-", "_"): value for key, value in data.items()}
 
-        if isinstance(normalized_data["tls_ca_chain"], str):
+        tls_ca_chain = normalized_data.get("tls_ca_chain")
+        if isinstance(tls_ca_chain, str):
             try:
-                normalized_data["tls_ca_chain"] = json.loads(normalized_data["tls_ca_chain"])
+                normalized_data["tls_ca_chain"] = json.loads(tls_ca_chain)
             except Exception:
                 pass
 
